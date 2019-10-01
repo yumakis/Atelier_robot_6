6e7c2a67fe4738cf25e536fe4098ff7cbe17ad0c
estellearrc <earricau@ensc.fr>
2019-10-01 18:07:32 +0200

    update classes

---
+++ motor.py
@@ -0,0 +1,5 @@
+class Motor():
+    def __init__(self, id):
+        self.id = id
+        #vitesse angulaire du robot en rad/s
+        self.w = 0
--- robot.py
+++ robot.py
@@ -1,19 +1,26 @@
+from motor import Motor
 
 class Robot():
-    #coordonnées du robot dans le repère du monde
-    x = 0
-    y = 0
-    theta = 0
-
-    #vitesse linéaire dans le repère du monde
-    vLin = 0
-    #vitesse angulaire dans le repère du monde
-    vTheta = 0
-
-    #coordonnées du robot dans le repère robot à l'instant t
     #on analyse une image de la vidéo tous les dt (t+1 = t + dt)
-    dx = 0
-    dy = 0
-    dTheta = 0
+    dt = 100 #en millisecondes
+    motorRight = Motor(2)
+    motorLeft = Motor(1)
 
+    def __init__(self):
+        #coordonnées du robot dans le repère du monde
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+
+        #vitesse linéaire dans le repère du monde
+        self.vLin = 0
+        #vitesse angulaire dans le repère du monde
+        self.vTheta = 0
+        
+        #coordonnées du robot dans le repère robot à l'instant t
+        self.dx = 0
+        self.dy = 0
+        self.dTheta = 0
     
+    def tick_odom(self):
+
