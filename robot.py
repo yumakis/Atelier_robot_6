103a7813171d32482c1cb618140e8ade3522044f
Terence <yumakis@github.com>
2019-10-02 14:10:09 +0200

    Correction caractères non ASCII

--- robot.py
+++ robot.py
@@ -8,17 +8,17 @@ class Robot():
     baseSpeed = 10 #vitesse en rpm
 
     def __init__(self):
-        #coordonnees du robot dans le repère du monde
+        #coordonnees du robot dans le repere du monde
         self.x = 0
         self.y = 0
         self.theta = 0
 
-        #vitesse lineaire dans le repère du monde
+        #vitesse lineaire dans le repere du monde
         self.vLin = 0
-        #vitesse angulaire dans le repère du monde
+        #vitesse angulaire dans le repere du monde
         self.vTheta = 0
 
-        #coordonnees du robot dans le repère robot à l'instant t
+        #coordonnees du robot dans le repere robot à l'instant t
         self.dx = 0
         self.dy = 0
         self.dTheta = 0
@@ -27,27 +27,27 @@ class Robot():
         self.motorRight = Motor(2)
         self.motorLeft = Motor(1)
 
-        #distance entre les 2 roues du robot en mètres
+        #distance entre les 2 roues du robot en metres
         self.d = 0.165
 
     def DK(self):
-        #convertit les vitesses angulaires (rad/s) du moteur gauche vG et du moteur droit vD dans le repère monde
-        #en vitesses lineaire (m/s) vLin et vitesse angulaire (rad/s) vTheta dans le repère monde
+        #convertit les vitesses angulaires (rad/s) du moteur gauche vG et du moteur droit vD dans le repere monde
+        #en vitesses lineaire (m/s) vLin et vitesse angulaire (rad/s) vTheta dans le repere monde
         vG = self.motorLeft.w
         vD = self.motorRight.w
         self.vLin = Motor.R*(vG + vD) / 2
         self.vTheta = Motor.R*(vG - vD) / (2 * self.d)
 
     def odom(self):
-        #calcule les deplacements dX, dY, et dTheta entre les instants t et t + dt dans le repère du robot
+        #calcule les deplacements dX, dY, et dTheta entre les instants t et t + dt dans le repere du robot
         self.dTheta = self.vTheta * Robot.dt
         dL = self.vLin * Robot.dt
-        #projection du deplacement dL dans le repère monde
+        #projection du deplacement dL dans le repere monde
         self.dx = dL * cos(self.theta + self.dTheta)
         self.dy = dL * sin(self.theta + self.dTheta)
 
     def tick_odom(self):
-        #MAJ des paramètres dans le repère monde
+        #MAJ des parametres dans le repere monde
         self.DK()
         self.odom()
         self.x += self.dx
