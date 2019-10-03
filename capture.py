8fbd69bbb5606f1d7f52cfdc0e4e4e85c30b6dbf
Terence <yumakis@github.com>
2019-10-03 18:21:04 +0200

    MAJ

--- capture.py
+++ capture.py
@@ -43,13 +43,13 @@ while(i<3):
 
     if difference_moyenne  <= 0:  # le vert domine pas
         transition = False
-        robot.baseSpeed -= 5
+        robot.baseSpeed -= 15
         print("suivre la ligne")
     else:
         if (difference_moyenne > 0) and (transition == False):
             transition = True
             # t_transition = time.clock()
-            robot.baseSpeed += 5
+            robot.baseSpeed += 15
             i = (i+1)
             print("changement de ligne")
 
