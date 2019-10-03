13c1215c16a1b73b31361be2c8cc9f16f82daced
Terence <yumakis@github.com>
2019-10-03 16:27:16 +0200

    test vision

--- capture.py
+++ capture.py
@@ -6,7 +6,7 @@ import time
 Width = 640.
 Height = 480.
 
-video_capture = cv2.VideoCapture(2)
+video_capture = cv2.VideoCapture(0)
 video_capture.set(3, Width)
 video_capture.set(4, Height)
 
@@ -17,29 +17,29 @@ while(i<3):
 
     ret,frame = video_capture.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-    cv2.imshow('frame',hsv)
+    # cv2.imshow('frame',hsv)
     #cv2.waitKey(1)
-    
+
     lower = np.array(np.matrix([[20,50,50],[80,100,100],[0,39,64],[110,50,50]]))
     upper = np.array(np.matrix([[60,200,200],[105,255,255],[20,255,255],[130,255,255]]))
-    
+
     mask_jaune = cv2.inRange(hsv,lower[1], upper[1])  # jaune
     mask_bleu = cv2.inRange(hsv,lower[2], upper[2])  # bleu
     mask_rouge = cv2.inRange(hsv,lower[3], upper[3])  # rouge
     mask_couleur= list((mask_jaune,mask_bleu,mask_rouge))
-    
-    
+
+
     mask_vert = cv2.inRange(hsv,lower[0], upper[0]) # creation du mask vert hsv
     res1 = cv2.bitwise_and(frame,frame, mask= mask_vert) # masque vert case depart
 
-    
+
     mask = mask_vert + mask_couleur[i]
     res = cv2.bitwise_and(frame,frame,mask= mask)
     difference_moyenne = np.mean(mask_vert)-np.mean(mask_couleur[i])  #difference de moyenne
     print("mask_vert", np.mean(mask_vert), "mask_",i, np.mean(mask_couleur[i]))
     print(i, "diff moy", difference_moyenne)
-    
-    if difference_moyenne  <= 0:  # le vert domine pas 
+
+    if difference_moyenne  <= 0:  # le vert domine pas
         transition = False
         print("suivre la ligne")
     else:
@@ -53,19 +53,19 @@ while(i<3):
     ret,thresh = cv2.threshold(gray_image,127,255,0)  # binarisation
 
     M = cv2.moments(thresh)  # detecter la ligne
-    
+
     if M["m00"] != 0:
         cX = int(M["m10"] / M["m00"])  #coordonnees du centre en x
         cY = int(M["m01"] / M["m00"])  #coordonnees du centre en y
-    
+
     else :
-        cX, cY = 0, 0  
+        cX, cY = 0, 0
 
     #print(cX)
     #print(cY)
     cv2.circle(res, (cX, cY), 5, (255, 255, 255), -1)
     cv2.putText(res, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    
+
     delta = (1/Width) * ( Width/2-cX)
 
     #print(delta)
@@ -76,13 +76,13 @@ while(i<3):
     if delta  <= -0.1:
         print("le robot doit tourner a droite")
 
-    # affichage de l image 
-        
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+    # affichage de l image
+
+    # cv2.imshow('frame',frame)
+    # cv2.imshow('mask',mask)
+    # cv2.imshow('res',res)
     k = cv2.waitKey(5) & 0xFF
-    if k == 35: 
+    if k == 35:
         break
 
 cv2.destroyAllWindows()
