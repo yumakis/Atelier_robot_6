a1c218625a1cca8fc13a84fad0ce2c32628392e8
Terence <yumakis@github.com>
2019-10-03 15:49:27 +0200

    Changement vision

--- capture.py
+++ capture.py
@@ -1,97 +1,88 @@
 import numpy as np
 import cv2
-#from robot import Robot
+import time
 
-WIDTH = 640
-HEIGHT = 480
 
-#robot = Robot()
+Width = 640.
+Height = 480.
 
-video_capture = cv2.VideoCapture(0)
-video_capture.set(3, WIDTH)
-video_capture.set(4, HEIGHT)
+video_capture = cv2.VideoCapture(2)
+video_capture.set(3, Width)
+video_capture.set(4, Height)
 
-countGreenStart = 0
+i = 0
+transition = False
 
-while(True and countGreenStart < 3):
-    #capture image
+while(i<3):
+
     ret,frame = video_capture.read()
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-
-    #lower_blue = np.uint8([0,39,64])
-    #upper_blue = np.uint8([20,255,255])
-
-    #lower_red = np.uint8([50,50,100])
-    #upper_red = np.uint8([255,255,130])
+    cv2.imshow('frame',hsv)
+    #cv2.waitKey(1)
     
-    #lower_green = np.uint8([41, 39, 64])
-    #upper_green = np.uint8([80, 255, 255])
-
-    #lower_yellow = np.uint8([10,0,100])
-    #upper_yellow = np.uint8([40,170,255])
-
-    lower = np.array(np.matrix([[30,100,100],[80,100,100],[0,39,64],[110,50,50]]))
-    upper = np.array(np.matrix([[70,255,255],[105,255,255],[20,255,255],[130,255,255]]))
-
-    #lower_range = np.uint8([0, 80, 0])
-    #upper_range = np.uint8([255, 255, 255])
-
-    mask = cv2.inRange(hsv,lower[countGreenStart], upper[countGreenStart])
-
-    # Remove noise
-    #kernel_erode = np.ones((4,4), np.uint8)
-    #eroded_mask = cv2.erode(mask, kernel_erode, iterations=1)
-    #kernel_dilate = np.ones((6,6), np.uint8)
-    #dilated_mask = cv2.dilate(eroded_mask, kernel_dilate, iterations=1)  #Dilatation
-    # Find the different contours
-    #contours, hierarchy = cv2.findContours(dilated_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Sort by area (keep only the biggest one)
-    #contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
-    #if len(contours) > 0:
-    #    M = cv2.moments(contours[0])
-        # Centroid
-    #    cx = int(M['m10']/M['m00'])
-    #    cy = int(M['m01']/M['m00'])
-    #    print("Centroid of the biggest area: ({}, {})".format(cx, cy))
-    #else:
-    #    print("No Centroid Found")
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    # Get the line center
+    lower = np.array(np.matrix([[20,50,50],[80,100,100],[0,39,64],[110,50,50]]))
+    upper = np.array(np.matrix([[60,200,200],[105,255,255],[20,255,255],[130,255,255]]))
     
+    mask_jaune = cv2.inRange(hsv,lower[1], upper[1])  # jaune
+    mask_bleu = cv2.inRange(hsv,lower[2], upper[2])  # bleu
+    mask_rouge = cv2.inRange(hsv,lower[3], upper[3])  # rouge
+    mask_couleur= list((mask_jaune,mask_bleu,mask_rouge))
+    
+    
+    mask_vert = cv2.inRange(hsv,lower[0], upper[0]) # creation du mask vert hsv
+    res1 = cv2.bitwise_and(frame,frame, mask= mask_vert) # masque vert case depart
+
+    
+    mask = mask_vert + mask_couleur[i]
+    res = cv2.bitwise_and(frame,frame,mask= mask)
+    difference_moyenne = np.mean(mask_vert)-np.mean(mask_couleur[i])  #difference de moyenne
+    print("mask_vert", np.mean(mask_vert), "mask_",i, np.mean(mask_couleur[i]))
+    print(i, "diff moy", difference_moyenne)
+    
+    if difference_moyenne  <= 0:  # le vert domine pas 
+        transition = False
+        print("suivre la ligne")
+    else:
+        if (difference_moyenne > 0) and (transition == False):
+            transition = True
+            i = (i+1)
+            print("changement de ligne")
+
+
     gray_image = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)  # conversion de l image en niveau de gris
     ret,thresh = cv2.threshold(gray_image,127,255,0)  # binarisation
 
-    contours, hierarchy = cv2.findContours(thresh.copy(),1, cv2.CHAIN_APPROX_NONE)
-
-    if len(contours) > 0:
-        c = max(contours, key=cv2.contourArea)
-        M = cv2.moments(c)
-
-        if int(M['m00']) == 0:
-            continue
-
     M = cv2.moments(thresh)  # detecter la ligne
-        
-        
-    cX = int(M["m10"] / M["m00"])  #coordonnees du centre en x
-    cY = int(M["m01"] / M["m00"])  #coordonnees du centre en y
-    print(cX, cY)
+    
+    if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])  #coordonnees du centre en x
+        cY = int(M["m01"] / M["m00"])  #coordonnees du centre en y
+    
+    else :
+        cX, cY = 0, 0  
+
+    #print(cX)
+    #print(cY)
     cv2.circle(res, (cX, cY), 5, (255, 255, 255), -1)
     cv2.putText(res, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     
+    delta = (1/Width) * ( Width/2-cX)
+
+    #print(delta)
+
+    if delta  >= 0.1:
+        print("le robot doit tourner a gauche")
+
+    if delta  <= -0.1:
+        print("le robot doit tourner a droite")
+
+    # affichage de l image 
+        
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('res',res)
-
-    # cx = int(M['m10']/M['m00'])
-    # cy = int(M['m01']/M['m00'])
-
-    #delta = Robot.coef * (cx - WIDTH/2)
-    #robot.move(Robot.baseSpeed + delta, Robot.baseSpeed - delta)
-
     k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+    if k == 35: 
         break
 
 cv2.destroyAllWindows()
